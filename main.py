be82102635d7f07af022131661fc445d6764e7af
Mkemp <M.g.r.kemp@students.uu.nl>
2021-09-15 11:40:13 +0200

    added testModels map and some example models

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import numpy as np
 import trimesh
 
 trimesh.util.attach_to_log()
-mesh = trimesh.load('models/m0/m0.off', force='mesh')
+mesh = trimesh.load('testModels/m0/m0.off', force='mesh')
 
 mesh.show()
