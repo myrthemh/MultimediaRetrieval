21a3d89df24c25e46bdb82d15411c853f154d0ba
Mkemp <M.g.r.kemp@students.uu.nl>
2021-09-15 11:54:40 +0200

    Added bunny.ply

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import trimesh
 
 trimesh.util.attach_to_log()
-mesh = trimesh.load('testModels/m0/m0.off', force='mesh')
+#mesh = trimesh.load('testModels/m0/m0.off', force='mesh')
+mesh = trimesh.load('testModels/bunny.ply', force='mesh')
 
 mesh.show()
