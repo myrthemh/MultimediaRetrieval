d6719910d65f16deab7f2dddfff03d80dd05230e
Mkemp <M.g.r.kemp@students.uu.nl>
2021-09-27 13:51:37 +0200

    calc center mass

--- preprocess.py
+++ preprocess.py
@@ -18,6 +18,19 @@ def scale_mesh(mesh, scale):
   mesh.apply_transform(matrix)
   return mesh
 
+def calc_center_mass(mesh):
+  meshVolume = 0
+  temp = (0, 0, 0)
+
+  for index, face in enumerate(mesh.faces):
+    center = (mesh.vertices[face[0]] + mesh.vertices[face[1]] + mesh.vertices[face[2]]) / 4
+    volume = np.dot(mesh.vertices[face[0]], np.cross(mesh.vertices[face[1]], mesh.vertices[face[2]])) / 6
+    meshVolume += volume
+    temp = center * volume
+  meshCenter = temp / meshVolume
+
+  return meshCenter
+
 def save_mesh(mesh, path):
   utils.ensure_dir(path)
   trimesh.exchange.export.export_mesh(mesh, path, file_type="off")
@@ -36,13 +49,13 @@ def normalize_mesh(path):
   mesh = trimesh.load(path, force='mesh')
 
   #Center the mass of the mesh on (0,0,0)
-  center_mass = mesh.center_mass
+  center_mass = calc_center_mass(mesh)
   mesh.apply_translation(-center_mass)
-  
+
   #Get the highest value we can scale with so it still fits within the unit cube
   scale_value = 1 / max(mesh.bounds.flatten())
   mesh = scale_mesh(mesh, scale_value)
-  
+
   # print(mesh.bounds)
   # print(mesh.center_mass)
   return mesh
@@ -51,21 +64,15 @@ def normalize_mesh(path):
 def process_all():
   #Perform all preprocessing steps on all meshes:
   df = pd.read_excel(utils.excelPath)
-  normalize_values_before = []
-  normalize_values_after = []
   for index, row in df.iterrows():
     path = row['path']
     mesh = trimesh.load(row['path'])
-    normalize_values_before.append(mesh.center_mass)
-    mesh = normalize_mesh(mesh)
-    normalize_values_after.append(mesh.center_mass)
+    refined_path = path[:11] + 'refined_' + path[11:]
+
     if row['subsampled_outlier']:
       mesh = subdivision.subdivide(mesh, utils.target_vertices)
     if row['supersampled_outlier']:
       mesh = subdivision.superdivide(mesh, utils.target_faces)
-      save_mesh(mesh, refined_path)
-    refined_path = path[:11] + 'refined_' + path[11:]
+
     save_mesh(mesh, refined_path)
-  avgs_before = [sum(vals) / len(normalize_values_before) for vals in zip(*normalize_values_before)]
-  avgs_after = [sum(vals) / len(normalize_values_after) for vals in zip(*normalize_values_after)]
-  print(avgs_before, avgs_after)
+
