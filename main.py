ce50473a25458545e1cccd5362f30fce53bd1b38
Mkemp <M.g.r.kemp@students.uu.nl>
2021-09-30 11:28:58 +0200

    Fixed colors and histograms of axis-aligned bounding boxes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import time
+from datetime import timedelta
 
 import pyrender
 import trimesh
@@ -13,14 +15,16 @@ trimesh.util.attach_to_log(level=logging.INFO)
 def render(meshes, showWireframe=True):
   scene = pyrender.Scene()
   for mesh in meshes:
+    colorvisuals = trimesh.visual.ColorVisuals(mesh, [200,200,200,255])
+    mesh.visual = colorvisuals
     mesh1 = pyrender.Mesh.from_trimesh(mesh, smooth=False)
     scene.add(mesh1)
   if showWireframe:
     for mesh in meshes:
-      # Add copy of mesh in wireframe mode
-      material = pyrender.Material()
       # mesh = scale_mesh(mesh, 1.001)
-      wireframe = pyrender.Mesh.from_trimesh(mesh, wireframe=True, smooth=False, material=material)
+      colorvisuals = trimesh.visual.ColorVisuals(mesh, [0,0,0,255])
+      mesh.visual = colorvisuals
+      wireframe = pyrender.Mesh.from_trimesh(mesh, wireframe=True, smooth=False)
       scene.add(wireframe)
   pyrender.Viewer(scene, use_raymond_lighting=True)
 
@@ -31,10 +35,20 @@ def step_1():
   render([mesh])
 
 
+step_1()
+start_time = time.monotonic()
+print("Analyze 1")
 analyze.filter_database(utils.originalDB, utils.excelPath)
+print("Preprocessing")
 preprocess.process_all()
+print("Analyze 2")
 analyze.filter_database(utils.refinedDB, utils.refinedexcelPath)
+print("Read Excel")
 originalDF = utils.read_excel(original=True)
 refinedDF = utils.read_excel(original=False)
+print("Save histograms")
 analyze.save_all_histograms(originalDF, utils.imagePath)
 analyze.save_all_histograms(refinedDF, utils.refinedImagePath)
+
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
