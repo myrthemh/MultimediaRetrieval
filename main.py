cfe26cbe3a56da00eb4ff3795527290ef4ab8d25
Wouter Castelein <wmcastelein@gmail.com>
2021-09-30 13:39:58 +0200

    Fixed wireframe atop solid

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ import utils
 import numpy as np
 trimesh.util.attach_to_log(level=logging.INFO)
 
+def scale_outward(mesh):
+  epsilon = 0.0002
+  normals = mesh.face_normals
+  for index, face in enumerate(mesh.faces):
+    for vertice in face:
+      mesh.vertices[vertice] = mesh.vertices[vertice] + epsilon * normals[index]
+  return mesh
 
 def render(meshes, showWireframe=True):
   scene = pyrender.Scene()
@@ -19,14 +26,9 @@ def render(meshes, showWireframe=True):
     mesh.visual = colorvisuals
     mesh1 = pyrender.Mesh.from_trimesh(mesh, smooth=False)
     scene.add(mesh1)
-  boxf_trimesh = trimesh.creation.box(extents=0.1*np.ones(3))
-  boxf_face_colors = np.random.uniform(size=boxf_trimesh.faces.shape)
-  boxf_trimesh.visual.face_colors = boxf_face_colors
-  boxf_mesh = pyrender.Mesh.from_trimesh(boxf_trimesh, smooth=False)
-  scene.add(boxf_mesh)
   if showWireframe:
     for mesh in meshes:
-      # mesh = scale_mesh(mesh, 1.001)
+      mesh = scale_outward(mesh)
       colorvisuals = trimesh.visual.ColorVisuals(mesh, [0,0,0,255])
       mesh.visual = colorvisuals
       wireframe = pyrender.Mesh.from_trimesh(mesh, wireframe=True, smooth=False)
@@ -39,20 +41,20 @@ def step_1():
   mesh = trimesh.load('testModels/db/0/m0/m0.off', force='mesh')
   render([mesh])
 
-
-start_time = time.monotonic()
+step_1()
+# start_time = time.monotonic()
 # print("Analyze 1")
 # analyze.filter_database(utils.originalDB, utils.excelPath)
 # print("Preprocessing")
 # preprocess.process_all()
 # print("Analyze 2")
 # analyze.filter_database(utils.refinedDB, utils.refinedexcelPath)
-print("Read Excel")
-originalDF = utils.read_excel(original=True)
-refinedDF = utils.read_excel(original=False)
-print("Save histograms")
-analyze.save_all_histograms(originalDF, utils.imagePath)
-analyze.save_all_histograms(refinedDF, utils.refinedImagePath)
+# print("Read Excel")
+# originalDF = utils.read_excel(original=True)
+# refinedDF = utils.read_excel(original=False)
+# print("Save histograms")
+# analyze.save_all_histograms(originalDF, utils.imagePath)
+# analyze.save_all_histograms(refinedDF, utils.refinedImagePath)
 
-end_time = time.monotonic()
-print(timedelta(seconds=end_time - start_time))
+# end_time = time.monotonic()
+# print(timedelta(seconds=end_time - start_time))
