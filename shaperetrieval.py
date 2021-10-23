3576dd8da663a1a5fb398bd34db85e3496e893ce
Mkemp <M.g.r.kemp@students.uu.nl>
2021-10-23 12:39:03 +0200

    small thingimajiggies

--- shaperetrieval.py
+++ shaperetrieval.py
@@ -10,7 +10,7 @@ from scipy.stats import wasserstein_distance
 import utils
 
 
-def compute_eucledian_distance(vector1, vector2):
+def compute_euclidean_distance(vector1, vector2):
   return norm(vector1 - vector2)
 
 
@@ -33,6 +33,8 @@ def find_similar_meshes(mesh_path):
   mesh = trimesh.load(mesh_path, force='mesh')
   mesh_info = analyze.fill_mesh_info(mesh, -1, "path", features=True)
   df = utils.read_excel(original=False)
+
+  # Get feature vector:
   single_vector = np.asarray([mesh_info[column] for column in utils.scal_features])
   histograms = np.asarray([mesh_info[column] for column in utils.hist_features])
   histogram_vector = [preprocess.sum_divide(x) for x in histograms]
@@ -47,13 +49,13 @@ def find_similar_meshes(mesh_path):
   with open(utils.emd_norm_vector_path, 'rb') as f:
     emd_vector = np.load(f)
     for index, row in df.iterrows():
-      if(mesh_path[-11:] == row['path'][-11:]):
+      if mesh_path[-11:] == row['path'][-11:]:
         continue
       other_single_vector = np.asarray(row[utils.scal_features_norm])
       other_histogram_vector = np.asarray(row[utils.hist_features_norm])
-      scalar_distance = compute_eucledian_distance(single_vector, other_single_vector)
+      scalar_distance = compute_euclidean_distance(single_vector, other_single_vector)
       hist_distances = [wasserstein_distance(histogram_vector[i], other_histogram_vector[i]) for i in range(len(histogram_vector))]
-      
+
       #Standardize histogram distances:
       hist_distances /= emd_vector
       distance = 0.5 * scalar_distance + sum(0.125 * hist_distances)
@@ -61,9 +63,8 @@ def find_similar_meshes(mesh_path):
   distances.sort(key=sortmethod)
   return distances
 
-# mesh_path = 'testModels/refined_db/0/m0/m0.off'
-# mesh = trimesh.load(mesh_path, force='mesh')
-# distances = find_similar_meshes(mesh_path)
+# mesh = trimesh.load('testModels/refined_db/0/m0/m0.off', force='mesh')
+# distances = find_similar_meshes(mesh)
 # mesh.show()
 # for dist in distances:
 #   meshx = trimesh.load(dist[1], force='mesh')
